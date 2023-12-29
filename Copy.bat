4e87dc892daf3734e660a9c838a0490a6e0bd6ea
Anthony Kerr <kerr01@allegheny.edu>
2023-12-29 14:11:40 -0500

    Start...

---
+++ Copy.bat
@@ -0,0 +1,5 @@
+mkdir "C:\Users\Anthony Kerr\Downloads\KMCSpaceFS"
+mkdir "C:\Users\Anthony Kerr\Downloads\KMCSpaceFS\x64"
+copy \\TEO\C\Users\Administrator\source\repos\KMCSpaceFS\src\KMCSpaceFS.inf "C:\Users\Anthony Kerr\Downloads\KMCSpaceFS\" /Y
+copy \\TEO\C\Users\Administrator\source\repos\KMCSpaceFS\src\KMCSpaceFS-VOL.inf "C:\Users\Anthony Kerr\Downloads\KMCSpaceFS\" /Y
+copy \\TEO\C\Users\Administrator\source\repos\KMCSpaceFS\out\build\x64-Release\KMCSpaceFS.sys "C:\Users\Anthony Kerr\Downloads\KMCSpaceFS\x64\" /Y
