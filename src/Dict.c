d8123f20d56342c1b52ed7ba85563489aa5a6dce
Tony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2026-07-11 17:17:54 -0400

    Backpedal...

--- src/Dict.c
+++ src/Dict.c
@@ -194,7 +194,7 @@ unsigned long long FindDictEntry(Dict* dict, char* table, unsigned long long tab
 			ExFreePool(Filename);
 			return 0;
 		}
-		for (unsigned long long j = 0; j < filenamelen + 1; j++)
+		for (unsigned long long j = 0; j < filenamelen; j++)
 		{
 			if (!((incmp((table[tableend + dict[o].filenameloc + j] & 0xff), (Filename[j] & 0xff)) || (((table[tableend + dict[o].filenameloc + j] & 0xff) == *"/") && ((Filename[j] & 0xff) == *"\\")))))
 			{
@@ -207,7 +207,7 @@ unsigned long long FindDictEntry(Dict* dict, char* table, unsigned long long tab
 					filename[j] = table[tableend + dict[o].filenameloc + j] & 0xff;
 				}
 			}
-			if (j == filenamelen && ((table[tableend + dict[o].filenameloc + j] & 0xff) == 255 || (table[tableend + dict[o].filenameloc + j] & 0xff) == 42) && dict[o].hash == hash)
+			if (j == filenamelen - 1 && ((table[tableend + dict[o].filenameloc + j + 1] & 0xff) == 255 || (table[tableend + dict[o].filenameloc + j + 1] & 0xff) == 42) && dict[o].hash == hash)
 			{
 				ExFreePool(Filename);
 				return o;
